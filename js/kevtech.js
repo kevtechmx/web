8dea7d15c655f85157ca9eb3859efd21e34322f5
kevtechmx <45611481+kevtechmx@users.noreply.github.com>
2018-12-04 18:06:19 -0600

    Primera Versión

---
+++ js/kevtech.js
@@ -0,0 +1,20 @@
+﻿
+/********************
+ *     Jquery       *
+ ********************/
+
+$(document).ready(function() {
+    // Efecto de Transicion navbar
+    $(window).scroll(function() {
+      // 
+      if($(this).scrollTop() > 100) { 
+          $('.kevtech-nav').addClass('bg-dark');
+          $('.kevtech-nav').removeClass('bg-transparent');
+          $('.kevtech-nav').removeClass('nav-big');
+      } else {
+          $('.kevtech-nav').removeClass('bg-dark');
+          $('.kevtech-nav').addClass('bg-transparent');
+          $('.kevtech-nav').addClass('nav-big');
+        }
+    });
+});
